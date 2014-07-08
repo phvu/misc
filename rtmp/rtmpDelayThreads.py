5b54d7633f0dd8579936443aec669d9bc7f6d759
phvu <phvu225@gmail.com>
2014-07-08 15:37:14 +0800

    updated rtmp

--- rtmp/rtmpDelayThreads.py
+++ rtmp/rtmpDelayThreads.py
@@ -1,37 +1,5 @@
-import subprocess, time, sys, threading, Queue
-
-def readUIntBE(s):
-    l = len(s)
-    assert l == 3 or l == 4
-    n = 0
-    for i in xrange(0, l):
-        n |= (ord(s[i]) << (l-i-1))
-    return n
-    
-def readUInt32BE(s):
-    assert len(s) == 4
-    return readUIntBE(s)
-
-def readUInt24BE(s):
-    assert len(s) == 3
-    return readUIntBE(s)
-
-def readHeader(stream):
-    data = stream.read(9)
-    assert data[:3] == 'FLV'
-    assert ord(data[3]) == 0x01
-    assert ord(data[4]) == 0x04      # audio
-    l = readUInt32BE(data[5:])
-    assert l >= 9
-    if l > 9:
-        data += stream.read(9 - l)
-    return data
-
-def readPackage(stream):
-    data = stream.read(15)
-    l = readUInt24BE(data[5:8])
-    data += stream.read(l)
-    return data
+import subprocess, time, sys, threading, Queue, datetime
+import rtmpDelay
 
 #########################################################################
 
@@ -52,13 +20,13 @@ class DumperThread (threading.Thread):
         pDumper = subprocess.Popen([RTMP_PATH, '-r', RTMP_URL, '--playpath=' + RTMP_PLAYPATH], \
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         
-        sData = readHeader(pDumper.stdout)
+        sData = rtmpDelay.readHeader(pDumper.stdout)
         self.qLock.acquire()
         self.qData.put(sData)
         self.qLock.release()
 
         while not exitFlag:
-            sData = readPackage(pDumper.stdout)
+            sData = rtmpDelay.readPackage(pDumper.stdout)
             self.qLock.acquire()
             self.qData.put(sData)
             self.qLock.release()
@@ -77,7 +45,12 @@ def run(delay):
 
         print 'Sleeping for %f secs... ' % delay,
         sys.stdout.flush()
-        time.sleep(delay)
+        
+        #time.sleep(delay)
+        iStart = datetime.datetime.now()
+        while (datetime.datetime.now() - iStart).total_seconds() < delay:
+            time.sleep(0)
+            
         print 'Here we go!'
         sys.stdout.flush()
         
@@ -92,7 +65,7 @@ def run(delay):
                 queueLock.release()
                 time.sleep(0.1)
     except:
-        print "Unexpected error:", sys.exc_info()[0]
+        print "Unexpected error:", sys.exc_info()
         sys.stdout.flush()
         exitFlag = 1
         dumperThread.join()
