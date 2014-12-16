621a8974b1f586eb6ef66b401f4dc21937a2a338
Vu Pham <phvu225@gmail.com>
2014-12-16 17:32:22 +0800

    updated editDistance

--- editDistance/editDistanceTest.py
+++ editDistance/editDistanceTest.py
@@ -2,14 +2,18 @@ import editDistance
 
 def test():
     E = editDistance.EditDistance()
-    (c, d, s) = E.minEditDistance('execution', 'intention')
+    (c, d, s) = E.minEditDistanceDebug('execution', 'intention')
     assert c == [1, 3, 1] and d == 8
-    (c, d, s) = E.minEditDistance('dog', 'cat')
+    assert E.minEditDistance('execution', 'intention') == d
+    (c, d, s) = E.minEditDistanceDebug('dog', 'cat')
     assert c == [0, 3, 0] and d == 6
-    (c, d, s) = E.minEditDistance('exclusive', 'excusi')
+    assert E.minEditDistance('dog', 'cat') == d
+    (c, d, s) = E.minEditDistanceDebug('exclusive', 'excusi')
     assert c == [3, 0, 0] and d == 3
-    (c, d, s) = E.minEditDistance([0, 1, 2, 3, 4], [1, 2, 3, 4, 5])
+    assert E.minEditDistance('exclusive', 'excusi') == d
+    (c, d, s) = E.minEditDistanceDebug([0, 1, 2, 3, 4], [1, 2, 3, 4, 5])
     assert c == [1, 0, 1] and d == 2
+    assert E.minEditDistance([0, 1, 2, 3, 4], [1, 2, 3, 4, 5]) == d
     
 if __name__ == '__main__':
     test()
